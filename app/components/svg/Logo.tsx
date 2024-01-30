5a5943415a24ffaac5309c82563278f3d5a23bc5
Muhammad Tufail Khan <124515869+TufailKhanDeveloper@users.noreply.github.com>
2024-01-30 18:49:12 +0500

    Update Logo.tsx

--- app/components/svg/Logo.tsx
+++ app/components/svg/Logo.tsx
@@ -10,7 +10,7 @@ const Logo: React.FC<LogoProps> = ({ width = 1000, height = 1000 }) => {
     return (
         <Image
             className={`w-${width} h-${height}`}
-            src="/Logo.png"
+            src="/Logo.svg"
             alt="Logo"
             width={width}
             height={height}
